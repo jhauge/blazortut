ebb9a21d40cf0b1ee251cdd4ef3463083f41ce5e
Jesper Hauge <jesper@iqit.dk>
2021-07-19 20:21:42 +0200

    Better calendar layout

--- Data/BookingService.cs
+++ Data/BookingService.cs
@@ -89,15 +89,91 @@ namespace BlazorTut.Data
             },
         };
 
+        private List<BookingWeek> barnweeks = new()
+        {
+            new BookingWeek()
+            {
+                WeekNumber = 30,
+                BookingDays = new[] {
+                    new BookingDay() { Date = new DateTime(2021, 7, 26), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 7, 27), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 7, 28), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 7, 29), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 7, 30), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 7, 31), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 1), IsBooked =  false, Account = ""}
+                }
+            },
+            new BookingWeek()
+            {
+                WeekNumber = 31,
+                BookingDays = new[] {
+                    new BookingDay() { Date = new DateTime(2021, 8, 2), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 3), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 4), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 5), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 6), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 7), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 8), IsBooked = false, Account = ""}
+                }
+            },
+            new BookingWeek()
+            {
+                WeekNumber = 32,
+                BookingDays = new[] {
+                    new BookingDay() { Date = new DateTime(2021, 8, 9), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 10), IsBooked = true, Account = "JM"},
+                    new BookingDay() { Date = new DateTime(2021, 8, 11), IsBooked = true, Account = "JM"},
+                    new BookingDay() { Date = new DateTime(2021, 8, 12), IsBooked = true, Account = "JM"},
+                    new BookingDay() { Date = new DateTime(2021, 8, 13), IsBooked = true, Account = "JM"},
+                    new BookingDay() { Date = new DateTime(2021, 8, 14), IsBooked = true, Account = "JM"},
+                    new BookingDay() { Date = new DateTime(2021, 8, 15), IsBooked = false, Account = ""}
+                }
+            },
+            new BookingWeek()
+            {
+                WeekNumber = 33,
+                BookingDays = new[] {
+                    new BookingDay() { Date = new DateTime(2021, 8, 16), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 17), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 18), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 19), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 20), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 21), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 22), IsBooked = false, Account = ""}
+                }
+            },
+            new BookingWeek()
+            {
+                WeekNumber = 35,
+                BookingDays = new[] {
+                    new BookingDay() { Date = new DateTime(2021, 8, 23), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 24), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 25), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 26), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 27), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 28), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 8, 29), IsBooked = false, Account = ""}
+                }
+            },
+            new BookingWeek()
+            {
+                WeekNumber = 36,
+                BookingDays = new[] {
+                    new BookingDay() { Date = new DateTime(2021, 8, 31), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 9, 1), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 9, 2), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 9, 3), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 9, 4), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 9, 5), IsBooked = false, Account = ""},
+                    new BookingDay() { Date = new DateTime(2021, 9, 6), IsBooked = false, Account = ""}
+                }
+            },
+        };
         public Task<List<BookingWeek>> GetBookingsAsync(string place)
         {
             if (place == "comm") return Task.FromResult(weeks);
-            List<BookingWeek> barnWeeks = weeks.ToList();
-            barnWeeks[0].BookingDays[4].IsBooked = false;
-            barnWeeks[0].BookingDays[5].IsBooked = false;
-            barnWeeks[0].BookingDays[6].IsBooked = false;
-            barnWeeks[1].BookingDays[0].IsBooked = false;
-            return Task.FromResult(barnWeeks);
+            return Task.FromResult(barnweeks);
         }
 
     }
